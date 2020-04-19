57b512fb571a3f5a5ddfa587d7009dac4bf44390
Amritanshu Pandey <amp.msit@gmail.com>
2020-04-19 18:59:55 +1000

    fix formatting

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-version = os.getenv('DRONE_TAG', 'ci')
+version = os.getenv("DRONE_TAG", "ci")
 
 requirements = [
     "Click>=7.0",
